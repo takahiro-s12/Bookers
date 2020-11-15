a2e5be74909b756f7162f80ec41c0f6d4044ce68
EC2 Default User <ec2-user@ip-172-31-84-244.ec2.internal>
2020-11-15 01:59:54 +0000

    [Finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,10 @@
 class BooksController < ApplicationController
+
+  def top
+  end
+
   def index
+    @book = Book.new
     @books = Book.all
   end
 
@@ -7,15 +12,14 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-    @books = Book.all
-  end
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/new'
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully"
+    else
+      render 'index'
+    end
   end
 
   def edit
@@ -23,18 +27,22 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      render 'edit'
+    end
   end
-  
+
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books/new'
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books', notice: "Book was successfully destroyed."
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'books#index'
-  get 'books' => 'books#new'
+  root to: 'books#top'
+  get 'books' => 'books#index'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   post 'books' => 'books#create'
